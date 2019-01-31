329062da6332172a70e633a0ab0fc0615d6723b8
XHermitOne <xhermitone@gmail.com>
2019-01-31 07:49:02 +0700

    Забыл зафиксировать

--- data_ctrl/postgresql_tab_wide.pas
+++ data_ctrl/postgresql_tab_wide.pas
@@ -13,7 +13,7 @@ uses
     tag_list;
 
 const
-  RESERV_PROPERTIES: Array [1..6] Of String = ('type', 'name', 'description', 'db_name', 'db_port', 'db_username', 'db_password', 'table_name');
+  RESERV_PROPERTIES: Array [1..8] Of String = ('type', 'name', 'description', 'db_name', 'db_port', 'db_username', 'db_password', 'table_name');
 
 type
   {
@@ -22,6 +22,25 @@ type
   TICPostgreSQLTableWide = class(TICObjectProto)
 
   private
+    FSQLQuery:       TSQLQuery;                     { коммандный процессор SQL }
+    FSQLTransaction: TSQLTransaction;               { координатор транзакций }
+    FPQConnection:   TPQConnection;                 { интерфейс PostgreSQL сервера }
+
+    {
+    Настроить параметры соединения с БД
+    @param aDBHost Сервер БД
+    @param aDBName Имя БД
+    @param aDBUserName Имя пользователя БД
+    @param aDBPassword Пароль пользователя БД
+    @return True - успешно / False - ошибка
+    }
+    function Connect(aDBHost: AnsiString; aDBName: AnsiString; aDBUserName: AnsiString; aDBPassword: AnsiString): Boolean;
+    {
+    Выполнить SQL выражение
+    @param aSQL Текст SQL выражения
+    @return True - успешно / False - ошибка
+    }
+    function ExecuteSQL(aSQL: AnsiString): Boolean;
 
   public
     constructor Create;
@@ -46,12 +65,72 @@ uses
 
 constructor TICPostgreSQLTableWide.Create;
 begin
-     inherited Create;
+  FSQLQuery := TSQLQuery.Create(nil);
+  FSQLTransaction := TSQLTransaction.Create(nil);
+  FPQConnection := TPQConnection.Create(nil);
+  inherited Create;
 end;
 
 procedure TICPostgreSQLTableWide.Free;
 begin
+  FPQConnection.Close;
+  FSQLQuery.Free;
+  FSQLTransaction.Free;
+  FPQConnection.Free;
+
   inherited Free;
+end;
+
+{ Настроить параметры соединения с БД }
+function TICPostgreSQLTableWide.Connect(aDBHost: AnsiString; aDBName: AnsiString; aDBUserName: AnsiString; aDBPassword: AnsiString): Boolean;
+begin
+  Result := True;
+  {подключение к SQL}
+  try
+    FPQConnection.HostName := aDBHost;
+    FPQConnection.DatabaseName := aDBName;
+    FPQConnection.UserName := aDBUserName;
+    FPQConnection.Password := aDBPassword;
+    FPQConnection.Open;
+
+    FSQLQuery.DataBase := FPQConnection;
+    FSQLQuery.Transaction := FSQLTransaction;
+    FSQLTransaction.DataBase := FPQConnection;
+    FPQConnection.Transaction := FSQLTransaction;
+  except
+    log.FatalMsg('Ошибка настройки параметров подключения к БД PostgreSQL');
+    Result := False
+  end;
+end;
+
+{ Выполнить SQL выражение }
+function TICPostgreSQLTableWide.ExecuteSQL(aSQL: AnsiString): Boolean;
+begin
+  Result := True;
+  try:
+    FPQConnection.Connected := True;
+  except
+    log.FatalMsg('Ошибка соединения с сервером PostgreSQL');
+    Result := False;
+  end;
+
+  try
+    FSQLTransaction.Active := True;
+  except
+    log.FatalMsg('Ошибка открытия транзакции');
+    Result := False;
+  end;
+
+  try
+    FSQLQuery.Close;
+    FSQLQuery.SQL.Clear;
+    //FSQLQuery.Insert;
+    FSQLQuery.SQL.Add(aSQL);
+    FSQLQuery.Open;
+  except
+    log.FatalMsg('Ошибка выполнения SQL выражения');
+    Result := False;
+  end;
 end;
 
 {
@@ -76,6 +155,7 @@ var
   i: Integer;
   key, value: AnsiString;
   tags: TStrDictionary;
+
 begin
   tags := TStrDictionary.Create;
   for i := 0 to Properties.Count - 1 do
@@ -87,7 +167,7 @@ begin
       tags.AddStrValue(key, value);
     end;
   end;
-  result := tags;
+  Result := tags;
 end;
 
 end.
