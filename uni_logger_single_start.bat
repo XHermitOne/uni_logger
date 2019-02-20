059adf6dbfb92f4e83e4e517798341caad023747
XhermitOne <xhermitone@yandex.ru>
2019-02-20 13:07:31 +0700

    Начало разработки альтернативного доступа к  OPC HDA серверу через DLL

--- uni_logger_single_start.bat
+++ uni_logger_single_start.bat
@@ -1,1 +1,2 @@
-D:\SPT\src\uni_logger\uni_logger_single.exe --log --settings=settings.ini
+D:\SPT\src\uni_logger\uni_logger_single.exe --log --debug
+rem D:\SPT\src\uni_logger\uni_logger_single.exe --log --settings=settings.ini
