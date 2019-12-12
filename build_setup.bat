25e11a17f46b545f5cb4570d8defae06b1e322a3
XHermitOne <xhermitone@gmail.com>
2019-12-12 14:02:18 +0700

    Пересборка инсталляционного пакета

--- build_setup.bat
+++ build_setup.bat
@@ -1,3 +1,3 @@
-"C:\Program Files\PeaZip\res\7z\7z.exe" a -sfx uni_logger_pack.exe logger.ico README.md settings.ini uni_logger.exe uni_logger_single.exe uni_logger_single_start.bat
+"C:\Program Files\PeaZip\res\7z\7z.exe" a -sfx uni_logger_pack.exe logger.ico README.md settings.ini uni_logger.exe uni_logger_single.exe uni_logger_single_start.bat *.txt
 move /Y uni_logger_pack.exe ".\packages\uni_logger_pack.exe"
 "C:\Program Files\NSIS\Bin\makensis.exe" uni_logger_install.nsi
